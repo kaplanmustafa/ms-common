1975e58c9fafa7e9aa1c035ea812979528b5701f
muskaplan <muskaplan@innova.com.tr>
2021-12-08 17:05:14 +0300

    initial commit

---
+++ src/test/java/com/example/mscommon/MsCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mscommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
