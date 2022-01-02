0377f35d381793e88997a6e94000eecb03ac207f
muskaplan <muskaplan@innova.com.tr>
2022-01-02 17:43:00 +0300

    feign handle exception

--- src/main/java/com/example/mscommon/error/ErrorAdvice.java
+++ src/main/java/com/example/mscommon/error/ErrorAdvice.java
@@ -1,5 +1,6 @@
 package com.example.mscommon.error;
 
+import com.example.mscommon.error.feign.MyFeignClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,12 @@ public class ErrorAdvice {
             rootError.addSubError(this.errconfig.buildBaseErrorObj().setMessage("" + objectErrorLoc).setErrorCode(12301));
         }
         return rootError;
+    }
+
+    @ExceptionHandler(MyFeignClientException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorObj handleException(final MyFeignClientException exp) {
+        return this.errconfig.buildBaseErrorObj().setMessage("Error while calling another MS").setErrorCode(18300).addSubError(exp.getErrorObj());
     }
 
     @ExceptionHandler(Exception.class)
---
+++ src/main/java/com/example/mscommon/error/feign/FeignErrorConfig.java
@@ -0,0 +1,13 @@
+package com.example.mscommon.error.feign;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignErrorConfig {
+
+    @Bean
+    public FeignErrorDecoder feignErrorDecoder() {
+        return new FeignErrorDecoder();
+    }
+}
